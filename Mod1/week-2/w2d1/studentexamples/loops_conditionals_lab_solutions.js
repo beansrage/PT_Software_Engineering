4b7af7239f4d9422d37779de9f794f8f14325f5a
Beans <pscbus_103@Beans-MBA.local>
2020-06-04 19:46:05 -0400

    current week homework

---
+++ Mod1/week-2/w2d1/studentexamples/loops_conditionals_lab_solutions.js
@@ -0,0 +1,9 @@
+// 1. How do we assign a value to a variable?
+
+// let a = 0; //Valid
+// const b = 1; //Valid
+// 9 = a; // Not valid
+// b = b + 5; // Not valid
+
+let firstVariable = 'Hello Word';
+    let 
---
+++ Mod1/week-2/w2d2/studentexamples/array_practice.js
@@ -0,0 +1,30 @@
+// const ghostBusters = ['Venkman', 'Spengler', 'Stantz', 'Zeddemore', 'Melnitz', 'Barrett', 'Tully'];
+// //console.log(ghostBusters[0], ghostBusters[2]);
+
+// //console.log(ghostBusters.length); // .length returns num of ELEMENTS
+// //console.log(ghostBusters.length - 1); .length -1 returns last in array
+// // ghostBusters[ghostBusters.length - 1] = 'Me'; /// to change an element in an array 
+// // console.log();
+// // console.log(ghostBusters);
+
+
+// const numbers = [21, 18, 5, 3, 2, 1, 1];
+// numbers[2] = null;
+// numbers [0] *= 10;
+// console.log(numbers);
+
+// const oddNumbered = ['first', 'second', 'third'];
+
+// console.log(oddNumbered[1]);
+// console.log(oddNumbered.length / 2);
+// console.log(Math.floor(oddNumbered.length / 2)); 
+// console.log(Math.floor(oddNumbered.length / 2)
+
+// )
+
+const kitchenSink = ['Dirty spoon', 'sponge', 'plate with gunk', 'soap', 'water'];
+for(let i =0; i < kitchenSink.length; i++){
+    console.log(i);
+    console.log('This is my kitchen sink ' + kitchenSink[i]);
+}
+
