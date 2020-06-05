95630b031e9d0ddbd2ee0949bbae9f8c754e4daa
Beans <pscbus_103@Beans-MBA.local>
2020-06-05 02:19:43 -0400

    Yell at the Ninja Turtles

--- Mod1/week-2/w2d2/homework/answers.js
+++ Mod1/week-2/w2d2/homework/answers.js
@@ -10,25 +10,59 @@ for(let i = 0; i < 21; i++){
 for (let i =0; i < 201; i+=2){
     console.log(i);
 }
+console.log('');
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+for (i =0; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('Fizz');
+    } else if (i % 3 === 0){
+        console.log('Buzz');
+    } else if (i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else {
+        console.log(i);
+    }
+}
 
-
+console.log('');
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-
-
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//1. 
+    plantee[2] = 5001;
+        console.log(plantee);
+//2.
+    wolfy[3] = 'Gotham City'
+        console.log(wolfy);
+//3. 
+    dart[4] = 'Hawkins'
+        console.log(dart);
+//4.
+    wolfy[0] = 'Gameboy'
+        console.log(wolfy);
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
-
-
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelango']
+    for (let char of ninjaTurtles){
+        console.log(char.toUpperCase(ninjaTurtles[i]));
+    }
+    
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 
+'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 
+'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 
+'Snatch', 'Fast and Furious'];
 
 
 ////////////////////////////////
