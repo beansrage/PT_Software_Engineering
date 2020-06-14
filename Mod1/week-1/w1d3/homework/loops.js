6688c4ca24d837147f21e0f977d1641f29be87f4
Beans <pscbus_103@Beans-MBA.local>
2020-06-14 18:56:02 -0400

    finished loop practice

---
+++ Mod1/week-1/w1d3/homework/loops.js
@@ -0,0 +1,25 @@
+//1. Create a loop that logs the numbers from 0-99 (ascending)
+    // for (let i = 0; i < 100; i++) {
+    //     console.log(i);
+    // }
+
+//2. Create a loop that logs the numbers from 99-0 (descending)
+    // for (let i = 100; i > 0; i--) {
+    //     console.log(i);
+    // }
+
+//3. Create a loop that logs the EVEN numbers from 0-98 (ascending)
+    // for (let i = 0; i < 100; i+=2) {
+    //         console.log(i);
+    // }
+
+//4. Create a loop that logs the EVEN numbers from 98-0 (descending)
+    //       for (let i = 100; i > 0; i-=2) {
+    //         console.log(i);
+    // }
+
+//5. Create a loop that logs the ODD numbers from 0-99 (ascending)
+    for (let i = 0; i < 100; i++) {
+        if (i % 2 !== 0)
+        console.log(i);
+    }
