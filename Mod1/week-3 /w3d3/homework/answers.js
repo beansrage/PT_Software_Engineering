fafa61809732809fc3c58da300e7ab4357dfba68
Beans <pscbus_103@Beans-MBA.local>
2020-06-15 06:43:48 -0400

    crazy object complete

--- Mod1/week-3 /w3d3/homework/answers.js
+++ Mod1/week-3 /w3d3/homework/answers.js
@@ -49,4 +49,47 @@
         faovriteDataType: 'OBJECTS!!!'
         
     }
-        console.log(Bob.hobby);
+        // console.log(Bob.hobby);
+
+
+////////Crazy Object!///////
+const crazyObject = {
+    taco: [{meat: 'steak',
+           cheese: ['panela', 'queso', 'chihuahua']},
+           {meat: 'chicken',
+            salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+          ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [{
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+      ]
+    }
+}
+//1.
+console.log(crazyObject.taco[1].salsa[5])
+
+//2.
+console.log(crazyObject.larry.quotes[0]);
+
+//3.
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+//4.
+console.log(crazyObject.larry.characters[1]);
+
+//5. 
+crazyObject.larry.quotes.push('I\'m trying to elevate small talk to medium talk')
+console.log(crazyObject.larry.quotes);
