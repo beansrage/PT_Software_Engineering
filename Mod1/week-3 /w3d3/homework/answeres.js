3e7374635a1e4347a21b1edea5a983c60adc2bce
Beans <pscbus_103@Beans-MBA.local>
2020-06-15 06:18:49 -0400

    datatype refresher complete

---
+++ Mod1/week-3 /w3d3/homework/answeres.js
@@ -0,0 +1,39 @@
+// 1. A light switch that can be either on or off.
+    // Datatype: Boolean. 
+    // ==> Data structure example: const lightswitch = true;
+
+//2.  A user's email address.
+     // Datatype: String
+    // ==> Data structure example: userName = {
+        //email: ==> 'e@amil.com', <===
+    // }
+
+//3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+    // Datatype: Object
+    // ==> Data structure example: spaceShip = {
+        // container: 'hull',
+        // weapon: 'laserblasters', 
+        // beam: 'tractor',
+        // drive: 'warp'
+    // }
+
+//4. A list of student names from our class.
+    // Datatype: Array
+    // ==> Data structure example: let classMates = ['Me', 'Tomas', 'Bruce', 'Sara','etc']
+
+// A list of student names from our class, each with a location.
+    // Datatype: Object
+    // ==> Data structure example: const classList = {
+        // { name: 'Mike', location: 'Earth'},
+        // { name: 'Jack', location: 'Earth'},
+        // { name: 'Sam', location: 'Earth'}
+    // }
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+       // Datatype: Object
+    // ==> Data structure example: const classList = {
+        // { name: 'Mike', location: 'Earth', favMovies : ['Superbad', 'MIB']},
+        // { name: 'Sam', location: 'Earth', favMovies : ['kjngfsjf', 'fd']},
+        // { name: 'Jon', location: 'Earth', favMovies : ['XXX', 'ttys']},
+    // }
+    
