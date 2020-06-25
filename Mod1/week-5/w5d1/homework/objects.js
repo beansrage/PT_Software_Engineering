75641df13768a192ae37634f95c3646f787b89fa
Beans <pscbus_103@Beans-MBA.local>
2020-06-25 02:15:46 -0400

    created hamster class

--- Mod1/week-5/w5d1/homework/objects.js
+++ Mod1/week-5/w5d1/homework/objects.js
@@ -15,20 +15,60 @@ getPrice() {
     }
 }
 
-// class Person {
-//     constructor(name){
-//     this.name = name
-//     this.age = 0
-//     this.height = 0
-//     this.mood = 0
-//     this.hamsters = []
-//     this.bankAccount = 0
-//     }
-// getName() {
-//     console.log(Person.name);
-//     }
-// }
-// getAge() {
+class Person {
+    constructor(name){
+    this.name = name
+    this.age = 0
+    this.height = 0
+    this.weight = 0
+    this.mood = 0
+    this.hamsters = []
+    this.bankAccount = 0
+    }
+getName() {
+    console.log(Person.name);
+    }
+getAge() {
+    console.log(Person.age); 
+}
+getWeight() {
+    console.log(Person.weight);
+}
+greet() {
+    console.log('Hello ' + Person.name + '. How are you?');
+}
+eat() {
+    for (let i = Person.weight; i <= 1; i++ ) {
+    }
+    for (let i = Person.mood; i <= 1; i++ ) {
+    }
+}
+exercise() {
+    for (let i = Person.weight; i <= 1; i-- ) {
+    }
+}
+ageUp() {
+    for (let i = Person.age; i <= 1; i++ ) {
+    }
+    for (let i = Person.height; i <= 1; i++ ) {
+    }
+    for (let i = Person.weight; i <= 1; i++ ) {
+    }
+    for (let i = Person.mood; i <= 1; i -= 10 ) {
+    }
+    for (let i = Person.bankAccount; i <= 1; i += 10 ) {
+    }
+}
+buyHamster(hamster) {
+    Hamster.push(Person.hamsters);
+    for (let i = Person.mood; i <= 1; i += 10 ) {
+    } 
+    for (let i = Person.bankAccount; i <= 1; i -- ) {
+        getPrice(); {
+            return Person.bankAccount - price; 
+        }
+    }
+}
+}
 
-// }
 
