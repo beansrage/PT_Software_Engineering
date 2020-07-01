d997582a2986539289ca8d3baf8e0877852c0a13
Beans <pscbus_103@Beans-MBA.local>
2020-07-01 12:59:31 -0400

    dougie and pizza rat have fought

--- Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
+++ Mod1/week-5/w5d1/homework/donut_adventure/adventure.js
@@ -14,10 +14,17 @@ class Hero {
        console.log(random);
     }   
     currenHP() {
+        console.log('Need... more... sprinkles');
         console.log(dMan.health);
     }
-    fight() {
+    fight(enemy) {
+        const damage = enemy.health -=this.weapons.sprinkleSpray;
         console.log('i\'m ready to rumble!');
+        console.log(this.weapons.sprinkleSpray);
+        console.log('');
+        console.log('I got him!, he should have ' + pRat.health + ' left');
+       
+        
     }
 }
 
@@ -29,18 +36,43 @@ class Enemy {
         pepperoniStars: 5,
         cheeseGrease: 10 
     }
-    this.catchPhrases = ['i\'m youtube famous',
-    'i\'m more dangerous than an uncovered sewer']
+        this.catchPhrases = ['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer']
     }
     talkSmack() {
         const random = this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)];
-       console.log(random);
+        console.log(random);
      } 
-     fight() {
-         console.log('i\'m gonna flatten you like a slice of pepperoni!');
-     }
+    currenHP() {
+        console.log('Feelin kinda low, better cheese it up!');
+        console.log(pRat.health);
+    }
+     fight(enemy) {
+        const damage = enemy.health -=this.weapons.pepperoniStars;  // decrementing dMans hp by value of specific weapon
+        console.log('i\'m gonna flatten you like a slice of pepperoni!');
+        console.log(this.weapons.pepperoniStars); 
+        console.log('');
+        console.log('Hit!, there should be ' + dMan.health + ' left');
+         
+        
+
+    }
 }
 const dMan = new Hero('Dougie');
 const pRat = new Enemy('Pizza Rat');
+console.log(dMan);
+console.log('');
+console.log(pRat);
+
+
+dMan.fight(pRat);
+console.log('');
+pRat.fight(dMan);
+
+console.log('');
+
+dMan.currenHP();
+console.log('');
+pRat.currenHP();
 
  
