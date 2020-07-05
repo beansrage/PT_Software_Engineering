b13c91f97709fffaba74d882a37f6279f60b4874
Beans <pscbus_103@Beans-MBA.local>
2020-07-05 03:34:51 -0400

    Commit 1 - I added my first element and it was fun

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,3 +1,14 @@
 // if (typeof jQuery == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+
+$(() => {
+    let $container = $('#container')	
+    console.log($container);
+    $h1 = document.createElement('H1');
+    let t = document.createTextNode('Hogwarts')
+    $h1.appendChild(t);
+    console.log($h1);
+    $container.append($h1)
+});
