8126c785b3db84d76421ab0b9b02f5d8a573c326
Beans <pscbus_103@Beans-MBA.local>
2020-07-19 05:54:20 -0400

    Commit 0 - All my files are linked and loaded

--- Mod1/week-5/w5d3/homework/hogwarts/app.js
+++ Mod1/week-5/w5d3/homework/hogwarts/app.js
@@ -1,103 +1,151 @@
-// if (typeof jQuery == 'undefined'){
-//     console.log('oops! I still have to link my jQuery properly!');
-//   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+                        //////////Jquery link check\\\\\\\\\\\\\
+                            //linked
+                    // if (typeof jQuery == 'undefined'){
+                    //     console.log('oops! I still have to link my jQuery properly!');
+                    // } else {console.log('I did it! I linked jQuery and this js file!')};
 
 
-$(() => {
-    $('div')
-    let $container = $('#container')	
-////////////H1
-    $h1 = document.createElement('H1');
-    let t = document.createTextNode('Hogwarts')
-    $container.append($h1)
-    $h1.append(t);
-////////////H2
-    $h2 = document.createElement('H2');
-    let tH2 = document.createTextNode('Bob')
-    $h2.appendChild(tH2);
-    $container.append($h2)
-/////////////H3
-    $h3 = document.createElement('H3');
-    let tH3 = document.createTextNode('Slytherin')
-    $h3.appendChild(tH3);
-    $container.append($h3)
-/////////////H4
-    $h4 = document.createElement('H4');
-    let tH4 = document.createTextNode('Zele')
-    $h4.className = 'Wolf'
-    $h4.appendChild(tH4);
-    $container.append($h4)
-/////////////H5
-    $h5 = document.createElement('H5')
-                ////Broken at year 5\\\
-    // let tH5 = document.createTextNode('Hawthorn Wand with Dragon Heartstring Core wand')
-    // $h5.appendChild(tH5);   
-    // $container.append($h5)
 
-    //////New wand!\\\\\\\\\\\
-    let tH5 = document.createTextNode('Holly Wand with Unicorn Hair Core')
-    $h5.style.color = '#4B0082'
-    $h5.appendChild(tH5);   
-    $container.append($h5)
+                    
+        
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+///////////////////////////old ways///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+// // if (typeof jQuery == 'undefined'){
+// //     console.log('oops! I still have to link my jQuery properly!');
+// //   } else {console.log('I did it! I linked jQuery and this js file!')};
+
+
+// $(() => {
+//     $('div')
+//     let $container = $('#container')	
+// ////////////H1
+//     $h1 = document.createElement('H1');
+//     let t = document.createTextNode('Hogwarts')
+//     $container.append($h1)
+//     $h1.append(t);
+// ////////////H2
+//     $h2 = document.createElement('H2');
+//     let tH2 = document.createTextNode('Bob')
+//     $h2.appendChild(tH2);
+//     $container.append($h2)
+// /////////////H3
+//     $h3 = document.createElement('H3');
+//     let tH3 = document.createTextNode('Slytherin')
+//     $h3.appendChild(tH3);
+//     $container.append($h3)
+// /////////////H4
+//     $h4 = document.createElement('H4');
+//     let tH4 = document.createTextNode('Zele')
+//     $h4.className = 'Wolf'
+//     $h4.appendChild(tH4);
+//     $container.append($h4)
+// /////////////H5
+//     $h5 = document.createElement('H5')
+//                 ////Broken at year 5\\\
+//     // let tH5 = document.createTextNode('Hawthorn Wand with Dragon Heartstring Core wand')
+//     // $h5.appendChild(tH5);   
+//     // $container.append($h5)
+
+//     //////New wand!\\\\\\\\\\\
+//     let tH5 = document.createTextNode('Holly Wand with Unicorn Hair Core')
+//     $h5.style.color = '#4B0082'
+//     $h5.appendChild(tH5);   
+//     $container.append($h5)
     
     
     
-//////////////////////////////
-//////////////////////////////
-/////////Year 3///////////////
-    $uL = document.createElement('ul');
-    document.getElementsByTagName('ul')[0]; //Get
-    let att = document.createAttribute('Storage');
-    att.value = 'trunk'; //Set
-    $uL.setAttributeNode(att);
-    $container.append($uL)
+// //////////////////////////////
+// //////////////////////////////
+// /////////Year 3///////////////
+//     $uL = document.createElement('ul');
+//     document.getElementsByTagName('ul')[0]; //Get
+//     let att = document.createAttribute('Storage');
+//     att.value = 'trunk'; //Set
+//     $uL.setAttributeNode(att);
+//     $container.append($uL)
     
-    //      'Removed" for year 5  \\
-    // $li = document.createElement('li');
-    // let lt = document.createTextNode('butter beer');
-    // $uL.append(lt)   
+//     //      'Removed" for year 5  \\
+//     // $li = document.createElement('li');
+//     // let lt = document.createTextNode('butter beer');
+//     // $uL.append(lt)   
     
     
-    $li2 = document.createElement('li');
-    let lt2 = document.createTextNode('invisibility cloak');
-    $li2.className = 'secret'
-    $uL.append($li2)
-    $li2.appendChild(lt2)
+//     $li2 = document.createElement('li');
+//     let lt2 = document.createTextNode('invisibility cloak');
+//     $li2.className = 'secret'
+//     $uL.append($li2)
+//     $li2.appendChild(lt2)
 
-    $li3 = document.createElement('li');
-    let lt3 = document.createTextNode('magic map');
-    $li3.className = 'secret'
-    $uL.append($li3)
-    $li3.appendChild(lt3)
+//     $li3 = document.createElement('li');
+//     let lt3 = document.createTextNode('magic map');
+//     $li3.className = 'secret'
+//     $uL.append($li3)
+//     $li3.appendChild(lt3)
 
-    $li4 = document.createElement('li');
-    let lt4 = document.createTextNode('time turner');
-    $li4.className = 'secret'
-    $uL.append($li4)
-    $li4.appendChild(lt4)
+//     $li4 = document.createElement('li');
+//     let lt4 = document.createTextNode('time turner');
+//     $li4.className = 'secret'
+//     $uL.append($li4)
+//     $li4.appendChild(lt4)
 
-    $li5 = document.createElement('li');
-    let lt5 = document.createTextNode('leash');
-    $li5.className = 'Wolf'
-    $uL.append($li5)
-    $li5.appendChild(lt5)
+//     $li5 = document.createElement('li');
+//     let lt5 = document.createTextNode('leash');
+//     $li5.className = 'Wolf'
+//     $uL.append($li5)
+//     $li5.appendChild(lt5)
 
-    $li6 = document.createElement('li');
-    let lt6 = document.createTextNode('Bertie Bott\'s Every Flavor [Jelly] Beans.');
-    $uL.append(lt6)
- //////////////////////////
- ///////////Year 4/////////
- //////////////////////////
+//     $li6 = document.createElement('li');
+//     let lt6 = document.createTextNode('Bertie Bott\'s Every Flavor [Jelly] Beans.');
+//     $uL.append(lt6)
+//  //////////////////////////
+//  ///////////Year 4/////////
+//  //////////////////////////
 
- //In html, not sure if we were supposed to create dynamically or what
-
-
- //////////////////////////
- ///////////Year 4/////////
- //////////////////////////
-document.removeChild(tH5);
+//  //In html, not sure if we were supposed to create dynamically or what
 
 
-});
+//  //////////////////////////
+//  ///////////Year 6/////////
+//  //////////////////////////
+//  $('H5').hide(slow)
 
 
+// });
