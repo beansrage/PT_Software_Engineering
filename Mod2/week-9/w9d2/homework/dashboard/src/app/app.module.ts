5a0aee410872548c4679f18d6a230467b884fa3e
Beans <pscbus_103@Beans-MBA.local>
2020-08-30 16:13:29 -0400

    created rating componenet

--- Mod2/week-9/w9d2/homework/dashboard/src/app/app.module.ts
+++ Mod2/week-9/w9d2/homework/dashboard/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule
