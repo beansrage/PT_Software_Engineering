130c65378501d7c1bd567c1e91f4c11a5183aba7
Beans <pscbus_103@Beans-MBA.local>
2020-08-30 16:33:42 -0400

    created sentiment-analysis

--- Mod2/week-9/w9d2/homework/dashboard/src/app/app.module.ts
+++ Mod2/week-9/w9d2/homework/dashboard/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { RatingComponent } from './rating/rating.component';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     ReviewsComponent,
-    RatingComponent
+    RatingComponent,
+    SentimentAnalysisComponent
   ],
   imports: [
     BrowserModule
